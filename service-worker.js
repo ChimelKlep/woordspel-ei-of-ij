2b48720ec085df9df738f5a506b0f2b97fefc12b
ChimelKlep <93780026+ChimelKlep@users.noreply.github.com>
2024-10-19 11:06:05 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,16 +2,23 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('ei-ij-cache').then(function(cache) {
             return cache.addAll([
-                'https://chimelklep.github.io/woordspel-ei-of-ij/',
-                'https://chimelklep.github.io/woordspel-ei-of-ij/index.html',
-                'https://chimelklep.github.io/woordspel-ei-of-ij/manifest.json',
-                'https://chimelklep.github.io/woordspel-ei-of-ij/service-worker.js',
-                'https://chimelklep.github.io/woordspel-ei-of-ij/icon-192.png',
-                'https://chimelklep.github.io/woordspel-ei-of-ij/icon-512.png',
-                'https://chimelklep.github.io/woordspel-ei-of-ij/zinnen.json'
+                'index.html',
+                'manifest.json',
+                'service-worker.js',
+                'icon-192.png',
+                'icon-512.png',
+                'zinnen.json'
             ]);
         }).catch(function(error) {
             console.error('Failed to cache files:', error);
+        })
+    );
+});
+
+self.addEventListener('fetch', function(event) {
+    event.respondWith(
+        caches.match(event.request).then(function(response) {
+            return response || fetch(event.request);
         })
     );
 });
