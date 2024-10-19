57a50cf0252429003fc46b345d73959ded9b1c90
ChimelKlep <93780026+ChimelKlep@users.noreply.github.com>
2024-10-19 10:37:21 +0200

    Add files via upload

--- service-worker.js
+++ service-worker.js
@@ -2,26 +2,16 @@ self.addEventListener('install', function(event) {
     event.waitUntil(
         caches.open('ei-ij-cache').then(function(cache) {
             return cache.addAll([
-                '/woordspel-ei-of-ij/',
-                '/woordspel-ei-of-ij/index.html',
-                '/woordspel-ei-of-ij/manifest.json',
-                '/woordspel-ei-of-ij/service-worker.js',
-                '/woordspel-ei-of-ij/icon-192.png',
-                '/woordspel-ei-of-ij/icon-512.png',
-                '/woordspel-ei-of-ij/zinnen.json',
-                '/woordspel-ei-of-ij/style.css',  // Voeg je CSS-bestanden toe
-                '/woordspel-ei-of-ij/script.js'  // Voeg je JS-bestanden toe
+                'https://chimelklep.github.io/woordspel-ei-of-ij/',
+                'https://chimelklep.github.io/woordspel-ei-of-ij/index.html',
+                'https://chimelklep.github.io/woordspel-ei-of-ij/manifest.json',
+                'https://chimelklep.github.io/woordspel-ei-of-ij/service-worker.js',
+                'https://chimelklep.github.io/woordspel-ei-of-ij/icon-192.png',
+                'https://chimelklep.github.io/woordspel-ei-of-ij/icon-512.png',
+                'https://chimelklep.github.io/woordspel-ei-of-ij/zinnen.json'
             ]);
         }).catch(function(error) {
             console.error('Failed to cache files:', error);
-        })
-    );
-});
-
-self.addEventListener('fetch', function(event) {
-    event.respondWith(
-        caches.match(event.request).then(function(response) {
-            return response || fetch(event.request);
         })
     );
 });
